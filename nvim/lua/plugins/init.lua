2620bd3b73e62e39277fac21ac899fc261ff18bf
Flavio Mili <flaviomilinanni@gmail.com>
2024-11-15 19:24:40 +0100

    Supermaven added

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,107 +1,11 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
-
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
-
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = {
-      git = { enable = true },
-    },
-  },
-
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "cpp",
-        "lua",
-        "python",
-        "vhdl",
-        "sql",
-      }
-    }
-  },
-
-  {
-    "smoka7/hop.nvim",
-    opts = {
-      multi_windows = true,
-      keys = "wertiopksjdmxnc",
-      uppercase_labels = false,
-    },
-    keys = {
-      {
-        "<leader>fj",
-        function ()
-          require("hop").hint_words()
-        end,
-        mode = { "n", "x", "o" }
-      }
-    }
-  },
-
-  {
-    'Exafunction/codeium.vim',
-    event = 'BufEnter',
-    version="1.8.37",
-  },
-
-  { 'echasnovski/mini.nvim', version = '*', lazy=false, config=function() require('mini.animate').setup() end},
-    {
-    "kkoomen/vim-doge", lazy=false
-  },
-{
-    "toppair/peek.nvim",
-    event = { "VeryLazy" },
-    build = "deno task --quiet build:fast",
-    config = function()
-        require("peek").setup({
-            app = 'browser',
-      })
-        vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-        vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
-    end,
-},
-{
-  'nvimdev/dashboard-nvim',
-  event = 'VimEnter',
-  config = function()
-    require('dashboard').setup {
-        theme = 'hyper',
-        config = {
-         header = {
-            '',
-          ' █████▒██▓    ▄▄▄    ██▒   █▓ ██▓ ███▄ ▄███▓',
-          '▓██   ▒▓██▒   ▒████▄ ▓██░   █▒▓██▒▓██▒▀█▀ ██▒',
-          '▒████ ░▒██░   ▒██  ▀█▄▓██  █▒░▒██▒▓██    ▓██░',
-          '░▓█▒  ░▒██░   ░██▄▄▄▄██▒██ █░░░██░▒██    ▒██ ',
-          '░▒█░   ░██████▒▓█   ▓██▒▒▀█░  ░██░▒██▒   ░██▒',
-          ' ▒ ░   ░ ▒░▓  ░▒▒   ▓▒█░░ ▐░  ░▓  ░ ▒░   ░  ░',
-          ' ░     ░ ░ ▒  ░ ▒   ▒▒ ░░ ░░   ▒ ░░  ░      ░',
-          ' ░ ░     ░ ░    ░   ▒     ░░   ▒ ░░      ░   ',
-          '           ░  ░     ░  ░   ░   ░         ░   ',
-          '                          ░                   ',
-          },
-         shortcut ={
-          {
-            desc = "Oh no!!! Not you again!!!",
-          },
-        },
-          footer = {"", "Let’s pretend this never happened!"},
-       },
-      }
-        end,
-        dependencies = { {'nvim-tree/nvim-web-devicons'}}
-  },
-
+  require("plugins.supermaven"),
+  require("plugins.dashboard"),
+  require("plugins.hop"),
+  require("plugins.peek"),
+  require("plugins.mini"),
+  require("plugins.conform"),
+  require("plugins.lspconfig"),
+  require("plugins.nvimtree"),
+  require("plugins.treesitter"),
 }
