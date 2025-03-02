3bffb9cedb2ff344c5ba5cb1a469b5c453231e7b
Flavio Mili <flaviomilinanni@gmail.com>
2025-03-02 14:55:57 +0100

    Added spotify support

--- .zshrc
+++ .zshrc
@@ -147,3 +147,5 @@ export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
+
+export PATH=$PATH:/Users/flavio/.spicetify
--- nvim/init.lua
+++ nvim/init.lua
@@ -36,4 +36,14 @@ vim.schedule(function()
   require "mappings"
 end)
 
+local on_attach = function(client, bufnr) end
+local lspconfig = require('lspconfig')
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+lspconfig.vhdl_ls.setup({
+  on_attach = on_attach,
+  capabilities = capabilities
+})
+
 vim.cmd("language en_US")
